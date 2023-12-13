2dd543fcafbd333d6c56a07eb0f0760ab2096747
Iris Yatskaer <iriska1160@gmail.com>
2023-12-13 13:34:35 +0200

    Add mailService

---
+++ apps/mail/services/emails.json.js
@@ -0,0 +1,58 @@
+export function emailJson() {
+  return emails;
+}
+
+const emails = [
+  {
+    id: "e101",
+    subject: "Miss you!",
+    body: "Would love to catch up sometimes",
+    isRead: false,
+    sentAt: 1551133930594,
+    removedAt: null,
+    from: "daniel@momo.com",
+    to: "user@appsus.com",
+  },
+
+  {
+   
+      id: 'e102',
+      subject: 'Hello!',
+      body: 'Would love to catch up sometimes',
+      isRead: false,
+      sentAt : 1551133930594,
+      removedAt : null,
+      from: 'iris@momo.com',
+      to: 'user@appsus.com'
+      
+      
+  },
+
+  {
+   
+    id: 'e103',
+    subject: 'Coding Academy!',
+    body: 'Would love to catch up sometimes',
+    isRead: false,
+    sentAt : 1551133930594,
+    removedAt : null,
+    from: 'momo@momo.com',
+    to: 'user@appsus.com'
+    
+    
+},
+
+{
+   
+  id: 'e104',
+  subject: 'Udemy',
+  body: 'Would love to catch up sometimes',
+  isRead: false,
+  sentAt : 1551133930594,
+  removedAt : null,
+  from: 'nadav@momo.com',
+  to: 'user@appsus.com'
+  
+  
+},
+];
--- apps/mail/services/mail.service.js
+++ apps/mail/services/mail.service.js
@@ -1,1 +1,69 @@
 // mail service
+
+import { utilService } from "../../../services/util.service.js";
+import { storageService } from "../../../services/async-storage.service.js";
+import { localStorageService } from "../../../services/storage.service.js";
+
+export const emailService = {
+  query,
+};
+
+const EMAIL_KEY = "emailDB";
+
+const loggedinUser = {
+  email: "user@appsus.com",
+  fullname: "Mahatma Appsus",
+};
+
+function query(key) {
+  return storageService.query(key).then((entities) => {
+    //add filter later
+    return entities;
+  });
+}
+
+function get(key, emailId) {
+  return storageService.get(key, emailId);
+}
+
+function remove(key, emailId) {
+  return storageService.remove(key, emailId);
+}
+
+function save(key, entity) {
+  if (entity.id) {
+    return storageService.put(key, entity);
+  } else {
+    return storageService.post(key, entity);
+  }
+}
+
+function getEmptyEmail(senderData, emailData) {
+  return {
+    id: "",
+    subject: emailData.subject,
+    body: emailData.body,
+    isRead: false,
+    sentAt: emailData.date,
+    removedAt: null,
+    from: senderData.from,
+    to: senderData.to,
+  };
+}
+
+// Private functions
+function _createEmails() {
+  let emails = localStorageService.loadFromStorage(EMAIL_KEY);
+
+  if (!emails || !emails.length) {
+    emails = bookJson();
+    localStorageService.saveToStorage(BOOK_KEY, books);
+  }
+}
+
+function _createEmail(senderData, emailData) {
+  const email = getEmptyEmail(senderData, emailData);
+  email.id = utilService.makeId();
+
+  return email;
+}
