68fcf3fb78e5815d17318035bd897353ac6cc129
Iris Yatskaer <iriska1160@gmail.com>
2023-12-15 23:54:50 +0200

    Add some styling to navbar and change theme in home page

--- cmps/AppHeader.jsx
+++ cmps/AppHeader.jsx
@@ -1,16 +1,28 @@
-const { Link, NavLink } = ReactRouterDOM
+const { Link, NavLink } = ReactRouterDOM;
 
 export function AppHeader() {
-
-    return <header className="app-header">
-        <Link to="/">
-            <h3>LOGO!</h3>
-        </Link>
-        <nav>
-            <NavLink to="/">Home</NavLink>
-            <NavLink to="/about">About</NavLink>
-            <NavLink to="/mail">Mail</NavLink>
-            <NavLink to="/note">Note</NavLink>
-        </nav>
+  return (
+    <header className="app-header">
+      <Link to="/">
+        <h3>
+          <i className="fa-regular fa-chess-knight"></i> APPSUS!
+          <i className="fa-solid fa-chess-knight"></i>
+        </h3>
+      </Link>
+      <nav>
+        <NavLink to="/">
+          Home <i className="fa-solid fa-house"></i>
+        </NavLink>
+        <NavLink to="/about">
+          About <i className="fa-regular fa-address-card"></i>
+        </NavLink>
+        <NavLink to="/mail">
+          Mail <i className="fa-regular fa-envelope"></i>
+        </NavLink>
+        <NavLink to="/note">
+          Note <i className="fa-regular fa-note-sticky"></i>
+        </NavLink>
+      </nav>
     </header>
+  );
 }
--- views/Home.jsx
+++ views/Home.jsx
@@ -1,6 +1,48 @@
+const { useState, useEffect } = React;
 
 export function Home() {
-    return <section className="home">
-        <h1>Welcome to home page!</h1>
+  const [backgroundColor, setBackgroundColor] = useState("#f8f8f8");
+  const [isAsideVisible, setAsideVisible] = useState(false);
+
+  const changeBgcTheme = () => {
+    setAsideVisible(!isAsideVisible);
+  };
+
+  const handleSquareClick = (color) => {
+    setBackgroundColor(color);
+    setAsideVisible(false);
+  };
+
+  return (
+    <section className="home" style={{ backgroundColor: backgroundColor }}>
+      <h1>Choose your theme page color!</h1>
+
+      <button onClick={changeBgcTheme}>
+        <i className="fa-solid fa-gear"></i>
+      </button>
+
+      <aside style={{ display: isAsideVisible ? "block" : "none" }}>
+        <div
+          className="square"
+          id="square1"
+          onClick={() => handleSquareClick("#85586f")}
+        ></div>
+        <div
+          className="square"
+          id="square2"
+          onClick={() => handleSquareClick("#c9bbcf")}
+        ></div>
+        <div
+          className="square"
+          id="square3"
+          onClick={() => handleSquareClick("#ceedc7")}
+        ></div>
+        <div
+          className="square"
+          id="square4"
+          onClick={() => handleSquareClick("#d2daff")}
+        ></div>
+      </aside>
     </section>
+  );
 }
